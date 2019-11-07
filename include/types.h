fd437e48d36848f66bfff23d8d193e6d7a6f99d0
ohwhatafool <drishti.serenity@gmail.com>
2019-11-07 23:35:09 +0100

    disabling BSP

--- include/types.h
+++ include/types.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define ENABLE_BSP	
+#define ENABLE_BSP
 /* #undef ENABLE_SUPERSAMPLING */
 
 #include <optional>
@@ -13,20 +13,20 @@
 using namespace cv;
 
 #ifdef _WIN32
-	using byte	= unsigned __int8;
-	using word	= unsigned __int16;
-	using dword	= unsigned __int32;
-	using qword	= unsigned __int64;
+using byte = unsigned __int8;
+using word = unsigned __int16;
+using dword = unsigned __int32;
+using qword = unsigned __int64;
 #else
-	using byte	= uint8_t;
-	using word	= uint16_t;
-	using dword	= uint32_t;
-	using qword	= uint64_t;
+using byte = uint8_t;
+using word = uint16_t;
+using dword = uint32_t;
+using qword = uint64_t;
 #endif
 
-static const double	Pi	= 3.1415926;			///< Pi number
-static const float	Pif	= 3.1415926f;			///< Pi number
+static const double Pi = 3.1415926;  ///< Pi number
+static const float Pif = 3.1415926f; ///< Pi number
 
 static const float Epsilon = 1E-4;
 
-#define RGB(r, g, b)  Vec3f((b), (g), (r))
+#define RGB(r, g, b) Vec3f((b), (g), (r))
